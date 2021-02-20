cbec76f399c4e0bb68414ac7ede8732c8af6fae1
arjun-krishna <arjunkrishna1306@gmail.com>
2021-02-20 15:11:40 -0500

    add Person model to database

--- school_app/models.py
+++ school_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    address = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    age = models.IntegerField(default=0)
