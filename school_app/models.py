19f11432422a5bf1c3869d6cb270c456f8b53088
arjun-krishna <arjunkrishna1306@gmail.com>
2021-02-20 15:32:28 -0500

    add methods for Person model

--- school_app/models.py
+++ school_app/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 
 class Person(models.Model):
+    # Parameters
     address = models.CharField(max_length=200)
     phone_number = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
     age = models.IntegerField(default=0)
+
+    # Methods
+    def __str__(self):
+        return self.name
 
 class Professor(models.Model):
     person = models.OneToOneField(
